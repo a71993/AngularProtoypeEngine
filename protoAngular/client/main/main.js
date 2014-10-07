fbf6f142d014c56e4b24878456feb3ec5283eeda
getterk <getter88@hotmail.com>
2014-10-07 02:50:49 +0000

    navigation bar notes and projects fix

--- protoAngular/client/main/main.js
+++ protoAngular/client/main/main.js
@@ -1,12 +1,27 @@
-(function (angular) {
+(function(angular) {
   "use strict";
-  angular.module('AngularProtoypeEngine.main', ['ui.router', 'AngularProtoypeEngine.main.note'])
-  .config(function ($stateProvider) {
-    $stateProvider
-      .state('AngularProtoypeEngine.main', {
-        url: '/main',
-        abstract: true,
-        templateUrl: 'main/main.tpl.html'
-      });
-  });
+  angular.module('AngularProtoypeEngine.main', ['ui.router', 
+    'AngularProtoypeEngine.main.note', 'AngularProtoypeEngine.main.projects.project'])
+    .config(function($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider
+        .when('/AngularProtoypeEngine/main', '/AngularProtoypeEngine/main/notes');
+        
+      $urlRouterProvider
+        .when('/AngularProtoypeEngine/main/projects', '/AngularProtoypeEngine/main/projects/project');  
+
+      $stateProvider
+        .state('AngularProtoypeEngine.main', {
+          url: '/main',
+          abstract: false,
+          templateUrl: 'main/main.tpl.html'
+        });
+
+      $stateProvider
+        .state('AngularProtoypeEngine.main.projects', {
+          url: '/projects',
+          abstract: false,
+          templateUrl:  'main/main.tpl.html',
+        });
+        
+    });
 }(angular));
