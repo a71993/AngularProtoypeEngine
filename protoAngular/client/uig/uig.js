515d4e8bdb1e8910e2e1ce33de017d1106f1354c
tinnu <kristiinapokk@gmail.com>
2014-10-22 12:26:46 +0300

    button example on uig page (using directives) #27

--- protoAngular/client/uig/uig.js
+++ protoAngular/client/uig/uig.js
@@ -1,6 +1,6 @@
-
-  angular.module('AngularProtoypeEngine.main.uig', ['ui.router'])
-  .config(function ($stateProvider) {
+var appUig = angular.module('AngularProtoypeEngine.main.uig', ['ui.router']);
+ // angular.module('AngularProtoypeEngine.main.uig', ['ui.router'])
+  appUig.config(function ($stateProvider) {
     $stateProvider
       .state('AngularProtoypeEngine.main.uig', {
         url: '/uig',
@@ -8,22 +8,37 @@
         controller: 'UigController'
       });
   })
-  .controller('UigController', function ($scope) {
+  appUig.controller('UigController', function ($scope) {
       $scope.oneAtATime = true;
 
       $scope.groups = [
-         {
+       {
           title: 'Buttons',
-          items: ['Button 1', 'Button 2']
-      
-          },
+          items: ['Button 1']
+        },
         {
           title: 'Tables',
-          items: ['Table 1', 'Table 2']
+          items: ['Table 1']
         },
         {
           title: 'Tabs',
           items: ['Tab 1', 'Tab 2']
+        },
+        {
+          title: 'Inputs',
+          items: ['Input - Big', 'Input - Small']
+        },
+         {
+          title: 'Form',
+          items: ['Form ']
+        },
+        {
+          title: 'Date',
+          items: ['Date']
+        },
+        {
+          title: 'Menu',
+          items: ['Dropdown ']
         }
       ];
 
@@ -31,6 +46,23 @@
         isFirstOpen: true
       };
       
+  });
+  appUig.directive("buttonOne", function() {
+    return {
+      restrict: "E",
+      templateUrl: "uig/button1.html",
+      controller: function($scope) {
+       
+
+     $scope.radioModel = 'Middle';
+    /*
+      $scope.checkModel = {
+      left: false,
+      middle: true,
+      right: false
+    };*/},
+      controllerAs: "button1C"
+    };
   });
 
   
