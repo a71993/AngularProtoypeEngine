451f6f52a62b36eddf7c9a8b6518a0162aee12c3
getterk <getter88@hotmail.com>
2014-10-27 09:35:04 +0000

    componentide mooduli laadimine project accordioni korvale

--- protoAngular/client/project/project.js
+++ protoAngular/client/project/project.js
@@ -1,5 +1,7 @@
+(function(angular) {
+  "use strict";
 
-angular.module('AngularProtoypeEngine.main.project', [])
+angular.module('AngularProtoypeEngine.main.project', ['AngularProtoypeEngine.main.project.components'])
   .config(function ($stateProvider) {
     $stateProvider
       .state('AngularProtoypeEngine.main.project', {
@@ -12,15 +14,22 @@ angular.module('AngularProtoypeEngine.main.project', [])
   .controller('ProjectController', function ($scope, $state) {
     $scope.oneAtATime = true;
 
+    $scope.projectName = "Project1";
     $scope.projectDatas = [];
     $scope.projectComponents = [];
     $scope.projectScreens = [];
+    $scope.selectedComponent = '';
+    
+    $scope.template='project/projectInfo.tpl.html';
   
     $scope.addData = function() {
       $state.go('AngularProtoypeEngine.main.jsonData');
     };
-    $scope.addComponent = function() {
-      $state.go('AngularProtoypeEngine.main.components');
+    $scope.setComponent = function(comp) {
+      $scope.selectedComponent = comp;
+      console.log($scope.selectedComponent)
     };
     $scope.addScreen = function() {};
 });
+
+}(angular));
