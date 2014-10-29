20febe35486f80260476f2bfc81109f43cf93e9c
a71993 <a71993@ut.ee>
2014-10-29 14:21:52 +0000

    conf

--- protoAngular/server/main/config.js
+++ protoAngular/server/main/config.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.DB_URL || 'mongodb://localhost/AngularProtoypeEngin
  * Include all your global env variables here.
 */
 module.exports = exports = function (app, express, routers) {
-  app.set('port', process.env.PORT || 15454);
+  app.set('port', process.env.PORT || 8080);
   app.set('base url', process.env.URL || 'http://localhost');
   app.use(morgan('dev'));
   app.use(bodyParser.json());
@@ -22,7 +22,6 @@ module.exports = exports = function (app, express, routers) {
   app.use('/note', routers.NoteRouter);
   app.use('/jsonData', routers.JsonDataRouter);
   app.use('/uiComponent', routers.UIComponentRouter);
-  app.use('/uiScreen', routers.UIScreenRouter);
   
   app.use(middle.logError);
   app.use(middle.handleError);
