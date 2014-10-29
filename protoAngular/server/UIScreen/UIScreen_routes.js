f8d4a6ed77c3c73f95ed1b7268c463a6f4605150
JaneJ <jane193@hot.ee>
2014-10-29 23:57:22 +0200

    getting screens from DB,saving new screen to DB, creating new screen #20 #21 #22

--- protoAngular/server/UIScreen/UIScreen_routes.js
+++ protoAngular/server/UIScreen/UIScreen_routes.js
@@ -4,58 +4,73 @@ var UIScreen = require('./UIScreen_model.js');
 
 module.exports = exports = function (router) {
 
-  router.param('uiScreen', function(req, res, next, id) {
-    var query = UIScreen.findById(id);
-  
-    query.exec(function (err, uiScreen){
-      if (err) { return next(err); }
-      if (!uiScreen) { return next(new Error("can't find screen")); }
-      
-      console.log(req.body);
-  
-      req.uiScreen = uiScreen;
-      return next();
+    router.param('uiScreen', function (req, res, next, id) {
+        var query = UIScreen.findById(id);
+
+        query.exec(function (err, uiScreen) {
+            if (err) {
+                return next(err);
+            }
+            if (!uiScreen) {
+                return next(new Error("can't find screen"));
+            }
+
+            console.log(req.body);
+
+            req.uiScreen = uiScreen;
+            return next();
+        });
     });
-  });
-  
-  router.get('/', function(req, res, next) {
-    UIScreen.find(function(err, uiScreen){
-      if(err){ return next(err); }
-  
-      res.json(uiScreen);
+
+    router.get('/', function (req, res, next) {
+        UIScreen.find(function (err, uiScreen) {
+            if (err) {
+                return next(err);
+            }
+
+            res.json(uiScreen);
+        });
     });
-  });
-  
-  router.post('/', function(req, res, next) {
-    var uiScreen = new UIScreen(req.body);
-    console.log(uiScreen);
-  
-    uiScreen.save(function(err, uiScreen){
-      if(err){ return next(err); }
-  
-      res.json(uiScreen);
+
+    router.post('/', function (req, res, next) {
+        var uiScreen = new UIScreen(req.body);
+        console.log(uiScreen);
+
+        uiScreen.save(function (err, uiScreen) {
+            if (err) {
+                return next(err);
+            }
+
+            res.json(uiScreen);
+        });
     });
-  });
-    
-  router.get('/:uiScreen', function(req, res) {
-    res.json(req.uiScreen);
-  });
-  
-  router.delete('/:uiScreen', function(req, res, next) {
-    UIScreen.remove(req.uiScreen ,function(err, uiScreen){
-      if(err){ return next(err); }
-      res.json({ message: 'Successfully deleted' });
+
+    router.get('/:uiScreen', function (req, res) {
+        res.json(req.uiScreen);
     });
-  });
-  
-  router.put('/:uiScreen', function(req, res, next) {
-    console.log(req.uiScreen);
-    req.uiScreen = req.body;    
-    console.log(req.body);
-    var uiScreen = new UIScreen(req.uiScreen);
-    // uiScreen.save(function(err, jsonData){
-    //   if(err){ return next(err); }
-    //   res.json({ message: 'Successfully updated' });
-    // });
-  });
-};
+
+    router.delete('/:uiScreen', function (req, res, next) {
+        UIScreen.remove(req.uiScreen, function (err, uiScreen) {
+            if (err) {
+                return next(err);
+            }
+            res.json({
+                message: 'Successfully deleted'
+            });
+        });
+    });
+
+    router.put('/:uiScreen', function (req, res, next) {
+        console.log('updating');
+        req.uiScreen.title = req.body.title;
+        req.uiScreen.content = req.body.content;
+        req.UIScreen.mainpage=req.body.mainpage;
+        req.uiScreen.save(function (err, uiScreen) {
+            if (err) {
+                return next(err);
+            }
+            res.json({
+                message: 'Successfully updated'
+            });
+        });
+    });
